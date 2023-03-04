1310fbfabe1331820a17da89ded529345bf1ad9e
chairilrafi11 <chairilrafi11@gmail.com>
2023-03-05 01:21:07 +0700

    Product Detail Helper

--- lib/presentations/component/text_usecase.dart
+++ lib/presentations/component/text_usecase.dart
@@ -5,53 +5,61 @@ class TextUsecase {
       case "mobile-legends":
         return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID (SERVER)</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Diamond</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Diamond akan masuk otomatis ke akun anda</li></ol>';
       case "call-of-duty-mobile":
-        return "assets/images/cod.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">Open ID</span>&nbsp;akun Call Of Duty Mobile Anda</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal Top Up</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Beli Sekarang</span></li><li>Lakukan Pembayaran&nbsp;<span style="font-weight: bolder;">sesuai yang dipilih</span></li><li>Pilihan Top Up Anda akan masuk secara otomatis</li></ol>';
       case "free-fire2":
-        return "assets/images/ff.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Diamond</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Diamond akan masuk otomatis ke akun anda</li></ol>';
       case "pubg-mobile":
         return "assets/images/pubg.png";
+        //////////////////////
       case "dragon-raja":
-        return "assets/images/dragon_raja.jpg";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Coupon</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Coupon akan masuk otomatis ke akun Anda.</li></ol>';
       case "genshin-impact2":
-        return "assets/images/genshin_impact.jpg";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID dan SERVER</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Genesis Crystal</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Genesis Crystal akan Masuk Otomatis ke Akun Anda</li></ol>';
       case "free-fire-max":
         return "assets/images/ff_max.png";
+        /////////////////
       case "tower-of-fantasy":
         return "assets/images/tower_of_fantasy.png";
+        ////////////////
       case "honkai-impact3":
-        return "assets/images/hokai_impact_3.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID&nbsp;</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal Crystal atau B Chip</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Crystal atau B Chip akan Masuk Otomatis ke Akun Anda</li></ol>';
       case "goddes-of-victory-nikke":
         return "assets/images/goddes_of_victory.png";
+        ////////////////////
       case "life-after":
-        return "assets/images/lifeafter.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID dan Server</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Credits</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Life After Credits akan masuk otomatis ke akun Anda.</li></ol>';
       case "never-after":
-        return "assets/images/never_after.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID&nbsp;</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Diamonds</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Diamonds akan masuk otomatis ke akun Anda.</li></ol>';
       case "marvel-super-war":
-        return "assets/images/marvel_super_war.jpg";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Credits</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Credits akan masuk otomatis ke akun Anda.</li></ol>';
       case "one-punch-man":
-        return "assets/images/one_punch_man.jfif";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID dan Server</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Coupons</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Coupons akan masuk otomatis ke akun Anda.</li></ol>';
       case "the-legend-of-neverland":
         return "assets/images/the_legend_of_neverland.png";
+        //////////////////////
       case "tom-and-jerry-chase":
-        return "assets/images/tom_and_jerry_chase.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Diamond</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Diamond akan masuk otomatis ke akun Anda.</li></ol>';
       case "super-sus":
         return "assets/images/super_sus.png";
+        /////////////////////
       case "echocalypse":
         return "assets/images/echocalypse.png";
+        ///////////////////
       case "sausage-man2":
-        return "assets/images/susage_man.png";
+        return '<ol><li>Masukkan <b>ID</b></li><li>Pilih <b>Nominal Candy</b></li><li>Pilih Metode Pembayaran</li><li>Masukan Nomor <b>WhatsApp </b>Kamu</li><li>Lakukan <b>Pembayaran</b></li><li>Candy Akan Masuk Otomatis Di Akun Anda.</li><p style="text-align: center; "><span style="font-weight: bolder; color: rgb(255, 156, 0); font-size: medium;">Top Up Buka 24 Jam</span><br></p></ol>';
       case "laplace-m2":
-        return "assets/images/laplace_m.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID&nbsp;</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Spirals</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Spirals akan Masuk Otomatis ke Akun Anda</li></ol>';
       case "valorant2":
-        return "assets/images/valorant.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Point</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Valorant Point akan masuk otomatis ke akun Anda.</li></ol>';
       case "league-of-legends-wr":
         return '<ol><li>Masukkan Riot&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal&nbsp;</span>Wild Core</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Wild Core akan masuk otomatis ke akun Anda.</li></ol>';
       case "arena-of-valor2":
-        return "assets/images/aov.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">Open ID</span>&nbsp;akun AOV Anda</li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal Top Up</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Beli Sekarang</span></li><li>Lakukan Pembayaran&nbsp;<span style="font-weight: bolder;">sesuai yang dipilih</span></li><li>Pilihan Top Up Anda akan masuk secara otomatis</li></ol>';
       case "ragnarok-m-eternal-love":
-        return "assets/images/ragnarok.png";
+        return '<ol><li>Masukkan&nbsp;<span style="font-weight: bolder;">ID</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Nominal Big Cat Coins</span></li><li>Pilih&nbsp;<span style="font-weight: bolder;">Metode Pembayaran</span></li><li>Tulis&nbsp;<span style="font-weight: bolder;">nomor WhatsApp</span>&nbsp;yg benar!</li><li>Klik&nbsp;<span style="font-weight: bolder;">Beli&nbsp;</span>&amp; lakukan Pembayaran</li><li>Big Cat Coin akan masuk otomatis ke akun Anda.</li></ol>';
       case "ragnarok-x":
         return "assets/images/ragnarok_x.jpg";
+        ///////////////////////
       default:
         return "";
     }
